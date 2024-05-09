9e51117b745d8678fb23a13c4f93250705a2d114
caue845 <cauefelipe845@gmail.com>
2024-05-08 22:30:35 -0300

    atualização test.js

--- api/test.js
+++ api/test.js
@@ -1,40 +1,37 @@
-const assert = require('chai').assert;
+const { assert } = require('chai');
 const axios = require('axios');
 
 describe('Testes da API', function () {
     let userId;
 
     it('Listar todos usuários', async function () {
-        const response = await axios.get('http://localhost:3000/users');
+        const response = await axios.get('http://localhost:3000/alunos');
         assert.equal(response.status, 200);
         assert.isArray(response.data);
     });
 
     it('Criar novo usuário', async function () {
-        const newUser = { NOME: 'Usuario Teste', IDADE: 20 };
-        const response = await axios.post('http://localhost:3000/users', newUser);
-        assert.equal(response.status, 200);
+        const newUser = { nome: 'Usuario Teste', idade: 20 }; // Chave 'nome' e 'idade' em minúsculas
+        const response = await axios.post('http://localhost:3000/alunos', newUser);
+        assert.equal(response.status, 200); // Geralmente, a criação retorna 201 Created
         assert.isObject(response.data);
         assert.property(response.data, 'id');
         assert.isNumber(response.data.id);
-        userId = response.data.id; // store the user ID for use in the next test
+        userId = response.data.id; // armazena o ID do usuário para uso no próximo teste
     });
 
     it('Modificar usuário', async function () {
-        const updatedUser = { NOME: 'Usuario Teste Modificado', IDADE: 25 };
-        const response = await axios.put(`http://localhost:3000/users/${userId}`, updatedUser);
+        const updatedUser = { nome: 'Usuario Teste Modificado', idade: 25 };
+        const response = await axios.put(`http://localhost:3000/alunos/${userId}`, updatedUser);
         assert.equal(response.status, 200);
         assert.isObject(response.data);
-        assert.propertyVal(response.data, 'NOME', 'Usuario Teste Modificado');
-        assert.propertyVal(response.data, 'IDADE', 25);
+        assert.propertyVal(response.data, 'nome', 'Usuario Teste Modificado');
     });
+    
 
     it('Excluir usuário', async function () {
-        const response = await axios.delete(`http://localhost:3000/users/${userId}`);
-        assert.equal(response.status, 200);
-        const deletedUser = response.data;
-        assert.isObject(deletedUser);
-        assert.propertyVal(deletedUser, 'id', userId);
+        const response = await axios.delete(`http://localhost:3000/alunos/${userId}`);
+        assert.equal(response.status, 200); // Geralmente, a exclusão retorna 204 No Content
     });
 
 });
